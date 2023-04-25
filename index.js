e4eff1f26a55452fac0e726c22d3c02e9d5beeb1
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-04-25 12:08:54 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+const names = ['Dan', 'Mark', 'Anthony', 'Michelle']
+let messages = []
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages
+}
+
+
+function countDown(num) {
+    let counter = num
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+//countDown(5)
